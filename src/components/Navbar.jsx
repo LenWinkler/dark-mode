9aaa7cf547616e8cb10849b6e63ac2a40683c923
LenWinkler <lenwinkler86@gmail.com>
2019-10-03 00:43:29 -0400

    mvp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useDarkMode from "../hooks/useDarkMode";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode();
   const toggleMode = e => {
+    console.log(darkMode);
     e.preventDefault();
     setDarkMode(!darkMode);
   };
